c3f471623af8d81be123389cfe8f6283abbf39be
BeastLP <92763555+BeastLP@users.noreply.github.com>
2021-10-19 01:48:06 +0200

    Add files via upload

---
+++ additional/header.php
@@ -0,0 +1,52 @@
+<div class="navbar-container bg-primary-3">
+      <nav class="navbar navbar-expand-lg justify-content-between navbar-dark border-bottom-0 bg-primary-3" data-sticky="top">
+        <div class="container">
+          <div class="col flex-fill px-0 d-flex justify-content-between">
+            <a class="navbar-brand mr-0 fade-page" href="index.html">
+              <img src="logo.png" alt="Logo" width="160" height="50">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+              <img class="icon navbar-toggler-open" src="assets/img/icons/interface/menu.svg" alt="menu interface icon" data-inject-svg />
+              <img class="icon navbar-toggler-close" src="assets/img/icons/interface/cross.svg" alt="cross interface icon" data-inject-svg />
+            </button>
+          </div>
+
+          <div class="collapse navbar-collapse col px-0 px-lg-2 flex-fill">
+            <div class="py-2 py-lg-0">
+              <ul class="navbar-nav">
+
+                <li class="nav-item">
+                  <a href="index.html" class="nav-link">Startseite</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                  <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown-grid" aria-expanded="false" aria-haspopup="true">Partner</a>
+                  <div class="dropdown-menu row">
+                    <div class="col-auto" data-dropdown-content>
+                      <div class="dropdown-grid-menu">
+                        <a href="./partner.html" class="dropdown-item fade-page">Partner-Übersicht</a>
+                        <a href="./beastlp.html" class="dropdown-item fade-page">BeastLP</a>
+                        <a href="./derhoffy.html" class="dropdown-item fade-page">DerHoffy</a>
+                        <a href="./thirio.html" class="dropdown-item fade-page">Thirio.de</a>
+                        <a href="./dark-news.html" class="dropdown-item fade-page">Dark-News.eu</a>
+                        <a href="./black-host.html" class="dropdown-item fade-page">Black-Host.eu</a>
+                        <a href="./musikbots.html" class="dropdown-item fade-page">Musikbots.de<span class="badge badge-primary ml-2">New</span></a>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+
+                <li class="nav-item">
+                  <a href="#" class="nav-link">Support</a>
+                </li>
+
+                <li class="nav-item">
+                  <a href="impressum.html" class="nav-link">Impressum</a>
+                </li>
+              
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
