912eb422879b75250e32400b3bc119aa3bebd55d
BeastLP <92763555+BeastLP@users.noreply.github.com>
2021-10-19 17:20:15 +0200

    sss

--- beastlp.php
+++ beastlp.php
@@ -4,38 +4,38 @@
 // Social des Partner
 $name = 'BeastLP.eu';
 $desc = ' Thirio.de ist ein Community-Server mit Fokus auf Gaming.';
-$img_url = 'https://pbs.twimg.com/profile_banners/1389691856292810752/1630875175/1500x500';
+$img_url = 'https://pbs.twimg.com/profile_banners/1183629310395146241/1610303914/1500x500';
 
 // Informationen
 $kat = 'Content-Creator';
-$leiter = 'BeastLP ;)';
+$leiter = 'BeastLP';
 $stand = '14.10.2021';
 
 // Daten
-$data_1 = '10000';
-$data_1_name = 'Twitter-Impressionen Ø/Mon';
+$data_1 = '7';
+$data_1_name = 'Projekte';
 
-$data_2 = '9';
-$data_2_name = 'Teammitglieder';
+$data_2 = '64';
+$data_2_name = 'Youtube-Abonnenten';
 
-$data_3 = '40';
-$data_3_name = 'Twitter-Follower';
+$data_3 = '61';
+$data_3_name = 'Twitter- Follower';
 
-$data_4 = '627';
-$data_4_name = 'Twitter-Profilaufrufe';
+$data_4 = '190';
+$data_4_name = 'Twitch-Follower';
 
 // Weitere Infos
 $aboutus = ' Wir sind ein Community-Server mit Fokus auf Gaming. Derzeit ist ein Minecraft-Netzwerk geplant welcher sich noch in der Entwickung befindet.';
 
 
 // Links
-$link1 = 'https://thirio.de';
+$link1 = 'https://beastlp.eu';
 $link1_img = 'https://dark-network.eu/assets/img/icons/theme/social/google.svg';
 
-$link2 = 'https://twitter.com/ThirioDE';
+$link2 = 'https://twitter.com/RealBeastLP';
 $link2_img = 'https://dark-network.eu/assets/img/icons/social/twitter.svg';
 
-$link3 = 'https://discord.thirio.de';
+$link3 = 'https://discord.com/invite/K6Y8GKU9hv';
 $link3_img = 'https://dark-network.eu/assets/img/logos/brand/Discord-Logo-White.svg';
 
 ?>
