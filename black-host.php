e3e6ed4cf86632595937fc6f0fc699374aaa9182
BeastLP <92763555+BeastLP@users.noreply.github.com>
2021-10-19 02:14:18 +0200

    tzjztjtjtj

--- black-host.php
+++ black-host.php
@@ -1,206 +1,71 @@
-<!doctype html>
-<html lang="en">
-
-  
-<!-- Bought by SimbaAddons -->
-<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
-<head>
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-52115242-16"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-    
-      gtag('config', 'UA-52115242-16');
-    </script>
-    <!-- Google Tag Manager -->
-    <script>
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      '../www.googletagmanager.com/gtm5445.html?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-KG38NH9');
-    </script>
-    <!-- End Google Tag Manager -->
-    <meta charset="utf-8">
-    <title>Partner - Black-Host.eu</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Partner - Black-Host.eu">
-    <style>
-      @keyframes hideLoader{0%{ width: 100%; height: 100%; }100%{ width: 0; height: 0; }  }  body > div.loader{ position: fixed; background: white; width: 100%; height: 100%; z-index: 1071; opacity: 0; transition: opacity .5s ease; overflow: hidden; pointer-events: none; display: flex; align-items: center; justify-content: center;}body:not(.loaded) > div.loader{ opacity: 1;}body:not(.loaded){ overflow: hidden;}  body.loaded > div.loader{animation: hideLoader .5s linear .5s forwards;  } /* Typing Animation */.loading-animation {width: 6px;height: 6px;border-radius: 50%;animation: typing 1s linear infinite alternate;position: relative;left: -12px;}@keyframes typing {0% {background-color: rgba(100,100,100, 1);box-shadow: 12px 0px 0px 0px rgba(100,100,100, 0.2),24px 0px 0px 0px rgba(100,100,100, 0.2);}25% {background-color: rgba(100,100,100, 0.4);box-shadow: 12px 0px 0px 0px rgba(100,100,100, 2),24px 0px 0px 0px rgba(100,100,100, 0.2);}75% {background-color: rgba(100,100,100, 0.4);box-shadow: 12px 0px 0px 0px rgba(100,100,100, 0.2),24px 0px 0px 0px rgba(100,100,100, 1);}}
-    </style>
-    <script type="text/javascript">
-      window.addEventListener("load", function () {    document.querySelector('body').classList.add('loaded');  });
-    </script>
-    <link href="assets/css/theme.min.css" rel="stylesheet" type="text/css" media="all" />
-    <link rel="preload" as="font" href="assets/fonts/Inter-UI-upright.var.woff2" type="font/woff2" crossorigin="anonymous">
-    <link rel="preload" as="font" href="assets/fonts/Inter-UI.var.woff2" type="font/woff2" crossorigin="anonymous">
-  </head>
-
-  <body>
-    <div class="loader">
-      <div class="loading-animation"></div>
-    </div>
-
-    <?php include 'additional/header.php' ?>
+<?php
 
 
-    <section class="bg-primary-alt header-inner">
-      <div class="container">
-        <div class="row my-3">
-          <div class="col">
-            <nav aria-label="breadcrumb">
-              <ol class="breadcrumb">
-                <li class="breadcrumb-item">
-                  <a href="partner.html">Partner-Übersicht</a>
-                </li>
-                <li class="breadcrumb-item">
-                  <a href="#">Black-Host.eu</a>
-                </li>
-              </ol>
-            </nav>
-          </div>
-        </div>
+// Social des Partner
+$name = 'Dark-News.eu';
+$desc = 'Dark-News eine News-Seite. Dort sind immer die neusten News zum Thema Gaming, Radio und vielem mehr zu finden.';
+$img_url = 'https://pbs.twimg.com/profile_banners/1389691856292810752/1630875175/1500x500';
 
-        <div class="row justify-content-center text-center">
-          <div class="col-lg-9 col-xl-8">
-            <h1>Black-Host.eu</h1>
-          </div>
-        </div>
-        <div class="row my-6 justify-content-between">
-          <div class="col-lg-7 col-xl-8 mb-3 mb-lg-0">
-            <br>
-            <img src="https://pbs.twimg.com/profile_banners/1228626052823625728/1619021905/1500x500" alt="Image" class="rounded">
-          </div>
-          <div class="col-lg-4 col-xl-3">
-            <div class="mb-3">
-              <h6 class="mb-1">Beschreibung</h6>
-              <p>
-                Black-Host.eu bietet dir eine perfekte und redunante Lösung dein Unternehmen dort Hosten zu können. 
-                Durch Black-Host.eu hast du eine leitungsstarke und ausfallfreie Umgebung für deine Server.
+// Informationen
+$kat = 'News';
+$leiter = 'BeastLP';
+$stand = '14.10.2021';
 
+// Daten
+$data_1 = '10';
+$data_1_name = 'Seitenaufrufe';
 
-              </p>
-            </div>
-            <div class="mb-3">
-              <h6 class="mb-1">Kategorie</h6>
-              <span>Hosting</span>
-            </div>
-            <div class="mb-3">
-              <h6 class="mb-1">Geleitet von:</h6>
-              <span><a href="https://twitter.com/SimbaAddons">Simba</a></span>
-            </div>
-            <div class="mb-3">
-              <h6 class="mb-1">Website</h6>
-              <a href="https:/black-host.eu" target="_blank">https:/black-host.eu/</a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="divider">
-        <img src="assets/img/dividers/divider-1.svg" alt="graphical divider" data-inject-svg />
-      </div>
-    </section>
+$data_2 = '20';
+$data_2_name = 'Partner';
 
-    <section>
-      <div class="container">
-        <div class="row mb-4">
-          <div class="col">
-            <h2>Statistiken (Stand 14.10.2021)</h2>
-          </div>
-        </div>
-        <div class="row justify-content-center">
-          <div class="col-6 mb-3 col-lg-3 mb-lg-0" data-aos="fade-up" data-aos-delay="100">
-            <span class="display-4 text-primary d-block" data-countup data-start="1" data-end="4317" data-duration="3" data-grouping="true"></span>
-            <span class="h6">Seitenaufufe</span>
-          </div>
-          <div class="col-6 mb-3 col-lg-3 mb-lg-0" data-aos="fade-up" data-aos-delay="200">
-            <span class="display-4 text-primary d-block" data-countup data-start="1" data-end="34" data-duration="3" data-grouping="true"></span>
-            <span class="h6">Partner</span>
-          </div>
-          <div class="col-6 mb-3 col-lg-3 mb-lg-0" data-aos="fade-up" data-aos-delay="300">
-            <span class="display-4 text-primary d-block" data-countup data-start="1" data-end="177" data-duration="3" data-grouping="true"></span>
-            <span class="h6">Twitter-Follower</span>
-          </div>
-          <div class="col-6 mb-3 col-lg-3 mb-lg-0" data-aos="fade-up" data-aos-delay="400">
-            <span class="display-4 text-primary d-block" data-countup data-start="1" data-end="99.9" data-decimal-places="1" data-duration="3" data-grouping="true" data-suffix="%"></span>
-            <span class="h6">Serveruptime</span>
-          </div>
-        </div>
-      </div>
-    </section>
+$data_3 = '48';
+$data_3_name = 'Twitter-Follower';
 
-    <section class="p-0" data-reading-position>
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-xl-7 col-lg-8 col-md-10">
-            <article class="article">
+$data_4 = '287';
+$data_4_name = 'Twitter-Profilaufrufe';
 
-              <h1>Was steckt hinter uns?</h1>
-              <br>
-              <h4>Eine Idee</h4>
-              <p>
-                Wir haben eine Idee, die wir stehts versuchen umzusetzen. Wir inovieren unsere Webseite und unser Konzept täglich.
-              </p>
-              <h4>Harte Arbeit</h4>
-              <p>
-                Wir arbeiten täglich um unser Unternehmen zu verbessern und richten uns dabei auch nach unseren Kundenwünschen.
-              </p>
-              <h4>Unser Ziel</h4>
-              <p>
-                Unser Ziel ist es, unseren Kunden dauerhaft leistungsstarke Dienste für einen guten Preis anbieten.
-              </p>
-              <h4>Erreichbarkeit</h4>
-              <p>
-                Durch unsere Monitoring Systeme, werden unsere Techniker sofort über einen Ausfall benachrichtigt.
-              </p>
-              <h4>Stehts wachsend</h4>
-              <p>
-                Wir gewinnen täglich neue Kunden und können uns somit stehts erweitern um Neu- sowie Bestandskunden mehr bieten zu können.
-              </p>
-              <h4>Unser Team</h4>
-              <p>
-                Unsere Team besteht aus erfahreren und hilfsbereiten Mitarbeitern um einen schnellen Support gewährleisten zu können.
-              </p>  
-            </article>
-          </div>
-        </div>
-      </div>
-    </section>
+// Weitere Infos
+$aboutus = '
+
+<br>
+<h4>Eine Idee</h4>
+<p>
+  Wir haben eine Idee, die wir stehts versuchen umzusetzen. Wir inovieren unsere Webseite und unser Konzept täglich.
+</p>
+<h4>Harte Arbeit</h4>
+<p>
+  Wir arbeiten täglich um unser Unternehmen zu verbessern und richten uns dabei auch nach unseren Kundenwünschen.
+</p>
+<h4>Unser Ziel</h4>
+<p>
+  Unser Ziel ist es, unseren Kunden dauerhaft leistungsstarke Dienste für einen guten Preis anbieten.
+</p>
+<h4>Erreichbarkeit</h4>
+<p>
+  Durch unsere Monitoring Systeme, werden unsere Techniker sofort über einen Ausfall benachrichtigt.
+</p>
+<h4>Stehts wachsend</h4>
+<p>
+  Wir gewinnen täglich neue Kunden und können uns somit stehts erweitern um Neu- sowie Bestandskunden mehr bieten zu können.
+</p>
+<h4>Unser Team</h4>
+<p>
+  Unsere Team besteht aus erfahreren und hilfsbereiten Mitarbeitern um einen schnellen Support gewährleisten zu können.
+</p> 
+
+';
 
 
-    <section class="has-divider">
-      <div class="container pt-3">
-        <div class="row justify-content-center">
-          <div class="col-xl-7 col-lg-8 col-md-10">
-            <hr>
-            <div class="d-flex align-items-center">
-              <span class="text-small mr-1">Alle Links:</span>
-              <div class="d-flex mx-2">
-                  <a href="https://black-host.eu" class="btn btn-round btn-primary mx-1" target="_blank">
-                      <img class="icon icon-sm" src="assets/img/icons/theme/social/google.svg" alt="web" data-inject-svg />
-                 </a>
-                  <a href="https://twitter.com/BlackHostEU" class="btn btn-round btn-primary mx-1" target="_blank">
-                  <img class="icon icon-sm" src="assets/img/icons/social/twitter.svg" alt="twitter" data-inject-svg />
-                </a>
-                <a href="https://dsc.gg/black-host" class="btn btn-round btn-primary mx-1" target="_blank">
-                  <img class="icon icon-sm" src="assets/img/logos/brand/Discord-Logo-White.svg" alt="discord" data-inject-svg />
-                </a>
-              </div>
-            </div>
-            <hr>
-          </div>
-        </div>
-      </div>
-    </section>
+// Links
+$link1 = 'https://dark-news.eu/';
+$link1_img = '';
 
+$link2 = 'https://twitter.com/DarkNewsEu';
+$link2_img = '';
 
-    <?php include 'additional/footer.php' ?>
+$link3 = 'https://discord.gg/bsCAR6dYAr';
+$link3_img = '';
 
-  </body>
+?>
 
-
-<!-- Bought by SimbaAddons -->
-</html>
+<?php include 'partner-template.php' ?>
