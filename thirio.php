edcbf51335a86b8a2f8f9526dc2ff56df78d5b3e
BeastLP <92763555+BeastLP@users.noreply.github.com>
2021-10-19 14:41:47 +0200

    sss

--- thirio.php
+++ thirio.php
@@ -1,182 +1,43 @@
-<!doctype html>
-<html lang="en">
-
-  
-<!-- Bought by SimbaAddons -->
-<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
-<head>
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-52115242-16"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-    
-      gtag('config', 'UA-52115242-16');
-    </script>
-    <!-- Google Tag Manager -->
-    <script>
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      '../www.googletagmanager.com/gtm5445.html?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-KG38NH9');
-    </script>
-    <!-- End Google Tag Manager -->
-    <meta charset="utf-8">
-    <title>Partner - Thirio.de</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Partner - Thirio.de">
-    <style>
-      @keyframes hideLoader{0%{ width: 100%; height: 100%; }100%{ width: 0; height: 0; }  }  body > div.loader{ position: fixed; background: white; width: 100%; height: 100%; z-index: 1071; opacity: 0; transition: opacity .5s ease; overflow: hidden; pointer-events: none; display: flex; align-items: center; justify-content: center;}body:not(.loaded) > div.loader{ opacity: 1;}body:not(.loaded){ overflow: hidden;}  body.loaded > div.loader{animation: hideLoader .5s linear .5s forwards;  } /* Typing Animation */.loading-animation {width: 6px;height: 6px;border-radius: 50%;animation: typing 1s linear infinite alternate;position: relative;left: -12px;}@keyframes typing {0% {background-color: rgba(100,100,100, 1);box-shadow: 12px 0px 0px 0px rgba(100,100,100, 0.2),24px 0px 0px 0px rgba(100,100,100, 0.2);}25% {background-color: rgba(100,100,100, 0.4);box-shadow: 12px 0px 0px 0px rgba(100,100,100, 2),24px 0px 0px 0px rgba(100,100,100, 0.2);}75% {background-color: rgba(100,100,100, 0.4);box-shadow: 12px 0px 0px 0px rgba(100,100,100, 0.2),24px 0px 0px 0px rgba(100,100,100, 1);}}
-    </style>
-    <script type="text/javascript">
-      window.addEventListener("load", function () {    document.querySelector('body').classList.add('loaded');  });
-    </script>
-    <link href="assets/css/theme.min.css" rel="stylesheet" type="text/css" media="all" />
-    <link rel="preload" as="font" href="assets/fonts/Inter-UI-upright.var.woff2" type="font/woff2" crossorigin="anonymous">
-    <link rel="preload" as="font" href="assets/fonts/Inter-UI.var.woff2" type="font/woff2" crossorigin="anonymous">
-  </head>
-
-  <body>
-    <div class="loader">
-      <div class="loading-animation"></div>
-    </div>
-
-    <?php include 'additional/header.php' ?>
+<?php
 
 
-    <section class="bg-primary-alt header-inner">
-      <div class="container">
-        <div class="row my-3">
-          <div class="col">
-            <nav aria-label="breadcrumb">
-              <ol class="breadcrumb">
-                <li class="breadcrumb-item">
-                  <a href="partner.html">Partner-Übersicht</a>
-                </li>
-                <li class="breadcrumb-item">
-                  <a href="#">Thirio.de</a>
-                </li>
-              </ol>
-            </nav>
-          </div>
-        </div>
+// Social des Partner
+$name = 'Thirio.de';
+$desc = ' Thirio.de ist ein Community-Server mit Fokus auf Gaming.';
+$img_url = 'https://pbs.twimg.com/profile_banners/1389691856292810752/1630875175/1500x500';
 
-        <div class="row justify-content-center text-center">
-          <div class="col-lg-9 col-xl-8">
-            <h1>Thirio.de</h1>
-          </div>
-        </div>
-        <div class="row my-6 justify-content-between">
-          <div class="col-lg-7 col-xl-8 mb-3 mb-lg-0">
-            <br>
-            <img src="https://pbs.twimg.com/profile_banners/1278757214568828935/1617444901/1500x500" alt="Image" class="rounded">
-          </div>
-          <div class="col-lg-4 col-xl-3">
-            <div class="mb-3">
-              <h6 class="mb-1">Beschreibung</h6>
-              <p>
-                Thirio.de ist ein Community-Server mit Fokus auf Gaming.
-              </p>
-            </div>
-            <div class="mb-3">
-              <h6 class="mb-1">Kategorie</h6>
-              <span>Community</span>
-            </div>
-            <div class="mb-3">
-              <h6 class="mb-1">Geleitet von:</h6>
-              <span><a href="beastlp.html">BeastLP</a> und <a href="derhoffy.html">DerHoffy</a></span>
-            </div>
-            <div class="mb-3">
-              <h6 class="mb-1">Website</h6>
-              <a href="https:/twitter.com/Thiriode" target="_blank">https:/twitter.com/Thiriode</a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="divider">
-        <img src="assets/img/dividers/divider-1.svg" alt="graphical divider" data-inject-svg />
-      </div>
-    </section>
+// Informationen
+$kat = 'Community';
+$leiter = 'BeastLP und DerHoffy';
+$stand = '14.10.2021';
 
-    <section>
-      <div class="container">
-        <div class="row mb-4">
-          <div class="col">
-            <h2>Statistiken (Stand 14.10.2021)</h2>
-          </div>
-        </div>
-        <div class="row justify-content-center">
-          <div class="col-6 mb-3 col-lg-3 mb-lg-0" data-aos="fade-up" data-aos-delay="100">
-            <span class="display-4 text-primary d-block" data-countup data-start="1" data-end="10000" data-duration="3" data-grouping="true"></span>
-            <span class="h6">Twitter-Impressionen Ø/Mon</span>
-          </div>
-          <div class="col-6 mb-3 col-lg-3 mb-lg-0" data-aos="fade-up" data-aos-delay="200">
-            <span class="display-4 text-primary d-block" data-countup data-start="1" data-end="9" data-duration="3" data-grouping="true"></span>
-            <span class="h6">Teammitglieder</span>
-          </div>
-          <div class="col-6 mb-3 col-lg-3 mb-lg-0" data-aos="fade-up" data-aos-delay="300">
-            <span class="display-4 text-primary d-block" data-countup data-start="1" data-end="40" data-duration="3" data-grouping="true"></span>
-            <span class="h6">Twitter-Follower</span>
-          </div>
-          <div class="col-6 mb-3 col-lg-3 mb-lg-0" data-aos="fade-up" data-aos-delay="400">
-            <span class="display-4 text-primary d-block" data-countup data-start="1" data-end="627" data-duration="3" data-grouping="true"></span>
-            <span class="h6">Twitter-Profilaufrufe</span>
-          </div>
-        </div>
-      </div>
-    </section>
+// Daten
+$data_1 = '10000';
+$data_1_name = 'Twitter-Impressionen Ø/Mon';
 
-    <section class="p-0" data-reading-position>
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-xl-7 col-lg-8 col-md-10">
-            <article class="article">
-             
-                <h1>Was steckt hinter uns?</h1>
-                <br>
-                <p>
-                   Wir sind ein Community-Server mit Fokus auf Gaming. Derzeit ist ein Minecraft-Netzwerk geplant welcher sich noch in der Entwickung befindet.
-                </p>
+$data_2 = '9';
+$data_2_name = 'Teammitglieder';
 
-            </article>
-          </div>
-        </div>
-      </div>
-    </section>
+$data_3 = '40';
+$data_3_name = 'Twitter-Follower';
+
+$data_4 = '627';
+$data_4_name = 'Twitter-Profilaufrufe';
+
+// Weitere Infos
+$aboutus = ' Wir sind ein Community-Server mit Fokus auf Gaming. Derzeit ist ein Minecraft-Netzwerk geplant welcher sich noch in der Entwickung befindet.';
 
 
-    <section class="has-divider">
-        <div class="container pt-3">
-          <div class="row justify-content-center">
-            <div class="col-xl-7 col-lg-8 col-md-10">
-              <hr>
-              <div class="d-flex align-items-center">
-                <span class="text-small mr-1">Alle Links:</span>
-                <div class="d-flex mx-2">
-                    <a href="https://thirio.de" class="btn btn-round btn-primary mx-1" target="_blank">
-                        <img class="icon icon-sm" src="assets/img/icons/theme/social/google.svg" alt="web" data-inject-svg />
-                   </a>
-                    <a href="https://twitter.com/ThirioDe" class="btn btn-round btn-primary mx-1" target="_blank">
-                    <img class="icon icon-sm" src="assets/img/icons/social/twitter.svg" alt="twitter" data-inject-svg />
-                  </a>
-                  <a href="https://discord.com/invite/6heUkWF" class="btn btn-round btn-primary mx-1" target="_blank">
-                    <img class="icon icon-sm" src="assets/img/logos/brand/Discord-Logo-White.svg" alt="Discord" data-inject-svg />
-                  </a>
-                </div>
-              </div>
-              <hr>
-            </div>
-          </div>
-        </div>
-      </section>
+// Links
+$link1 = 'https://thirio.de';
+$link1_img = '';
 
-      <?php include 'additional/footer.php' ?>
+$link2 = 'https://twitter.com/ThirioDE';
+$link2_img = '';
 
-  </body>
+$link3 = 'https://discord.thirio.de';
+$link3_img = '';
 
+?>
 
-<!-- Bought by SimbaAddons -->
-</html>
+<?php include 'partner-template.php' ?>
