7dc117bef9098997f9e1fd20dfb2691dd7c281fa
BeastLP <92763555+BeastLP@users.noreply.github.com>
2021-10-19 16:43:50 +0200

    d

--- beast.php
+++ beast.php
@@ -2,40 +2,40 @@
 
 
 // Social des Partner
-$name = 'Musikbots.de';
-$desc = 'Musikbots.de ist ein Free-Musikbot Hosting';
+$name = 'BeastLP.eu';
+$desc = ' Thirio.de ist ein Community-Server mit Fokus auf Gaming.';
 $img_url = 'https://pbs.twimg.com/profile_banners/1389691856292810752/1630875175/1500x500';
 
 // Informationen
-$kat = 'Hosting';
-$leiter = 'BeastLP';
+$kat = 'Content-Creator';
+$leiter = 'BeastLP ;)';
 $stand = '14.10.2021';
 
 // Daten
-$data_1 = '7206';
+$data_1 = '10000';
 $data_1_name = 'Twitter-Impressionen Ø/Mon';
 
-$data_2 = '17';
-$data_2_name = 'Partner';
+$data_2 = '9';
+$data_2_name = 'Teammitglieder';
 
-$data_3 = '18';
+$data_3 = '40';
 $data_3_name = 'Twitter-Follower';
 
-$data_4 = '1059';
+$data_4 = '627';
 $data_4_name = 'Twitter-Profilaufrufe';
 
 // Weitere Infos
-$aboutus = 'Beschreibung folgt...';
+$aboutus = ' Wir sind ein Community-Server mit Fokus auf Gaming. Derzeit ist ein Minecraft-Netzwerk geplant welcher sich noch in der Entwickung befindet.';
 
 
 // Links
-$link1 = 'https://musikbots.de/';
+$link1 = 'https://thirio.de';
 $link1_img = 'https://dark-network.eu/assets/img/icons/theme/social/google.svg';
 
-$link2 = 'https://twitter.com/MusikbotsDE';
+$link2 = 'https://twitter.com/ThirioDE';
 $link2_img = 'https://dark-network.eu/assets/img/icons/social/twitter.svg';
 
-$link3 = 'https://musikbots.de/discord';
+$link3 = 'https://discord.thirio.de';
 $link3_img = 'https://dark-network.eu/assets/img/logos/brand/Discord-Logo-White.svg';
 
 ?>
