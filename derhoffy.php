edca8752d51ff4510d8576deea53550312886b0f
BeastLP <92763555+BeastLP@users.noreply.github.com>
2021-10-21 23:08:51 +0200

    shgdzuhj

---
+++ derhoffy.php
@@ -0,0 +1,42 @@
+<?php
+
+$name = 'DerHoffy';
+$desc = ' schreibt...';
+$img_url = 'https://pbs.twimg.com/profile_banners/1288233960133230592/1634849867/1500x500'
+$link = 'https://Instagram.com/hoffy._._';
+
+// Informationen
+$kat = 'Content-Creator';
+$leiter = 'DerHoffy';
+$stand = '21.10.2021';
+
+// Daten
+$data_1 = '3';
+$data_1_name = 'Projekte';
+
+$data_2 = '54';
+$data_2_name = 'Youtube-Abonnenten';
+
+$data_3 = '322';
+$data_3_name = 'Instagram-Abonnenten';
+
+$data_4 = '50';
+$data_4_name = 'Twitch-Follower';
+
+// Weitere Infos
+$aboutus = ' schreibt....';
+
+
+// Links
+$link1 = 'https://Instagram.com/hoffy._._';
+$link1_img = 'https://dark-network.eu/assets/img/icons/social/instagram.svg';
+
+$link2 = 'https://twitter.com/DerHoffyYT';
+$link2_img = 'https://dark-network.eu/assets/img/icons/social/twitter.svg';
+
+$link3 = 'https://discord.com/invite/K6Y8GKU9hv';
+$link3_img = 'https://dark-network.eu/assets/img/logos/brand/Discord-Logo-White.svg';
+
+?>
+
+<?php include 'partner-template.php' ?>
